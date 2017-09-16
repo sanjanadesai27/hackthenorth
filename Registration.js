970e282709312bc0d84179de0b6dd1cba67538de
Lenmor Ld <lenmorld@gmail.com>
2017-09-16 15:45:28 -0400

    registration post working

--- Registration.js
+++ Registration.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, TextInput, View, ScrollView,
-  Modal, TouchableHighlight, Button
+  Modal, TouchableHighlight, Button, Image
  } from 'react-native';
 
 import { Form,
@@ -8,6 +8,8 @@ import { Form,
   SwitchField, PickerField,DatePickerField,TimePickerField
 } from 'react-native-form-generator';
 
+
+APP_LOGO = "https://facebook.github.io/react/img/logo_og.png";
 
 // this could be in a seperate file too
 // class TextField extends Component {
@@ -47,21 +49,21 @@ class Registration extends Component {
     in this example.
     */
 
-    formData = {
-      first_name:"",
-      last_name:"",
-      gender: '',
-      country: '',
-      city: '',
-      birthday: Date,
-      blood_type: '',
-      allergies: '',
-      major_diagnosis: '',
-      notes: ''
-      // has_accepted_conditions: bool
-    }
+    // formData = {
+    //   first_name:"",
+    //   last_name:"",
+    //   gender: '',
+    //   country: '',
+    //   city: '',
+    //   birthday: Date,
+    //   blood_type: '',
+    //   allergies: '',
+    //   major_diagnosis: '',
+    //   notes: ''
+    //   // has_accepted_conditions: bool
+    // }
 
-    this.setState({formData:formData})
+    this.setState({formData:formData});
     this.setMessageVisible(!this.state.modalVisible);   // hide message again
     this.props.onFormChange && this.props.onFormChange(formData);
   }
@@ -107,76 +109,87 @@ class Registration extends Component {
 
   render() {
     return (
-      <ScrollView keyboardShouldPersistTaps="always"
-        style={{paddingLeft:10,paddingRight:10,paddingTop:10,paddingBottom:10, height:'100%', width: '100%'}}>
+        <ScrollView keyboardShouldPersistTaps="always"
+          style={{paddingLeft:10,paddingRight:10,paddingTop:10,paddingBottom:10, height:'100%', width: '100%'}}>
+          <Image
+            style={{width: 50, height: 50}}
+            source={{uri: APP_LOGO}}
+          />
 
-      <Form
-          ref='registrationForm'
-          // onFocus={this.handleFormFocus.bind(this)}
-          // onChange={this.handleFormChange.bind(this)}
+        <Form
+            ref='registrationForm'
+            // onFocus={this.handleFormFocus.bind(this)}
+            onChange={this.handleFormChange.bind(this)}
+            label="Personal Information">
+          <InputField ref='first_name' placeholder='First Name'/>
+          <InputField ref='last_name' placeholder='Last Name'/>
+          {/* <Separator /> */}
+          <PickerField style={[{backgroundColor: '#61d062'}]}
+            ref='gender'
+            label='Gender'
+            options={{
+              "": '',
+              male: 'Male',
+              female: 'Female',
+              other: 'Other'
+            }}/>
+          <InputField ref='country' placeholder='Country'/>
+          <InputField ref='city' placeholder='City'/>
 
-          label="Personal Information">
-        <Separator />
-        <InputField ref='first_name' placeholder='First Name'/>
-        <InputField ref='last_name' placeholder='Last Name'/>
-        {/* <Separator /> */}
-        <PickerField
-          ref='gender'
-          label='Gender'
-          options={{
-            "": '',
-            male: 'Male',
-            female: 'Female',
-            other: 'Other'
-          }}/>
-        <InputField ref='country' placeholder='Country'/>
-        <InputField ref='city' placeholder='City'/>
+          {/* <InputField
+            multiline={true}
+            ref='other_input'
+            placeholder='Other Input'
+            helpText='this is an helpful text it can be also very very long and it will wrap' /> */}
 
-        {/* <InputField
-          multiline={true}
-          ref='other_input'
-          placeholder='Other Input'
-          helpText='this is an helpful text it can be also very very long and it will wrap' /> */}
+          {/* <LinkField label="test test test" onPress={()=>{}}/> */}
+          {/* <SwitchField label='I accept Terms & Conditions'
+            ref="has_accepted_conditions"
+            helpText='Please read carefully the terms & conditions'/> */}
 
-        {/* <LinkField label="test test test" onPress={()=>{}}/> */}
-        {/* <SwitchField label='I accept Terms & Conditions'
-          ref="has_accepted_conditions"
-          helpText='Please read carefully the terms & conditions'/> */}
+            <DatePickerField
+              ref='birthday'
+              minimumDate={new Date('1/1/1900')}
+              maximumDate={new Date()}
+              placeholder='Birthday'/>
 
-          <DatePickerField ref='birthday'
-          minimumDate={new Date('1/1/1900')}
-          maximumDate={new Date()}
-          placeholder='Birthday'/>
+            <InputField ref='blood_type' placeholder='Blood Type'/>
+            <InputField ref='allergies' placeholder='Allergies (comma-separated values)'/>
 
-          <InputField ref='blood_type' placeholder='Blood Type'/>
-          <InputField ref='allergies' placeholder='Allergies (comma-separated values)'/>
-          <InputField ref='major_diagnosis' placeholder='Major Diagnosis'/>
-          <InputField ref='notes' placeholder='Notes'/>
+            <InputField ref='major_diagnosis'
+                      multiline={true}
+                      placeholder='Major Diagnosis'/>
 
+            {/* <InputField ref='notes' placeholder='Notes'/> */}
 
-        {/* <TimePickerField ref='alarm_time'
-      placeholder='Set Alarm'/> */}
-        {/* <DatePickerField ref='meeting'
-          minimumDate={new Date('1/1/1900')}
-          maximumDate={new Date()} mode="datetime" placeholder='Meeting'/> */}
-        </Form>
-
-        {/* onPress={this.handleFormChange.bind(this)} */}
-
-        <Button
-          onPress={this.registerPatient.bind(this)}
-          title="Register Patient"
-          color="#841584"
-          accessibilityLabel="Register Patient"
-        />
-
-        <Text>{JSON.stringify(this.state.formData)}</Text>
-        <Text style={styles.message}>
-          {this.state.message}
-        </Text>
-      </ScrollView>);
+            <InputField
+              multiline={true}
+              ref='notes'
+              placeholder='Notes (Specify all other patient info)' />
 
 
+          {/* <TimePickerField ref='alarm_time'
+        placeholder='Set Alarm'/> */}
+          {/* <DatePickerField ref='meeting'
+            minimumDate={new Date('1/1/1900')}
+            maximumDate={new Date()} mode="datetime" placeholder='Meeting'/> */}
+          </Form>
+
+          {/* onPress={this.handleFormChange.bind(this)} */}
+
+          <Button
+            onPress={this.registerPatient.bind(this)}
+            title="Register Patient"
+            color="#841584"
+            accessibilityLabel="Register Patient"
+          />
+
+          <Text>{JSON.stringify(this.state.formData)}</Text>
+          <Text style={styles.message}>
+            {this.state.message}
+          </Text>
+        </ScrollView>
+      );
     // return (
     //   // Try setting `alignItems` to 'flex-start'
     //   // Try setting `justifyContent` to `flex-end`.
